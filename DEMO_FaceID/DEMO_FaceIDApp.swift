1838c5cd36f000e4d1d743fa88fc1a9dc7f07875
Duong Jonathan <djonathan91.77@gmail.com>
2023-11-20 13:52:54 +0100

    Initial project

---
+++ DEMO_FaceID/DEMO_FaceIDApp.swift
@@ -0,0 +1,17 @@
+//
+//  DEMO_FaceIDApp.swift
+//  DEMO_FaceID
+//
+//  Created by Jonathan Duong on 20/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct DEMO_FaceIDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
